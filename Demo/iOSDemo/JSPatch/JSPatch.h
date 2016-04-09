1383405d447d1af816d2eb0abfb8cc014f5dc952
zeng zhihui <zengzhihui@score.cool>
2016-04-09 22:19:34 +0800

    add framework

---
+++ Demo/iOSDemo/JSPatch/JSPatch.h
@@ -0,0 +1,19 @@
+//
+//  JSPatch.h
+//  JSPatch
+//
+//  Created by zzh on 16/4/9.
+//  Copyright © 2016年 bang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSPatch.
+FOUNDATION_EXPORT double JSPatchVersionNumber;
+
+//! Project version string for JSPatch.
+FOUNDATION_EXPORT const unsigned char JSPatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSPatch/PublicHeader.h>
+
+
